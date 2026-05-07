ee428889e3183ef31a361d54cfcf63d8a526e0db
Sebastian Castillo <sebacastillo728@gmail.com>
2026-05-07 16:43:48 -0300

    Agregar archivos de proyecto.

---
+++ TaskListManager.Api/Entities/MyTask.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskListManager.Api.Entities
+{
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; } = null;
+        public bool IsCompleted { get; set; }
+        public int Priority { get; set; }
+        public int TaskListId { get; set; }
+    }
+}
---
+++ TaskListManager.Api/Entities/TaskList.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskListManager.Api.Entities
+{
+    public class TaskList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; } = null;
+        public List<MyTask> Tasks { get; set; } = new List<MyTask>();
+    }
+}
